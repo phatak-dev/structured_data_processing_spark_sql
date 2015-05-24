2d17451472c7b3a46c36aff6db1a5040c1ad29fb
madhukar <phatak.dev@gmail.com>
2015-05-24 17:13:41 +0530

    parquet example added

---
+++ src/main/scala/com/madhukaraphatak/spark/sql/parquet/AggParquet.scala
@@ -0,0 +1,18 @@
+package com.madhukaraphatak.spark.sql.parquet
+
+import org.apache.spark.SparkContext
+
+/**
+  * Aggregate using DataFrame API in Parquet file
+  */
+object AggParquet {
+
+   def main(args: Array[String]) {
+
+     val sc = new SparkContext(args(0), "Parquet loading example")
+     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+     val df = sqlContext.load("org.apache.spark.sql.parquet", Map("path" -> args(1), "header" -> "true"))
+     val countDF = df.agg(("transactionId", "count"))
+     println(countDF.collectAsList())
+   }
+ }
